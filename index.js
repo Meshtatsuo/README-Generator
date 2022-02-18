6d2e1b6a8dcdd8fd790bd3f18f80a2a278095839
MesH <meshtatsuo@gmail.com>
2022-02-17 17:42:05 -0700

    began adding questions for inquirer

--- index.js
+++ index.js
@@ -3,7 +3,50 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "project-title",
+        message: "What is the name of your project?",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "project-description",
+        message: "Please enter a brief description of your project.",
+        validage: (input) => {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter a description!");
+            return false;
+          }
+        },
+      },
+      {
+        // NEXT QUESTION
+      },
+    ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return;
+  `
+  Placeholder License Badge
+  `;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return;
+  `
+  Placeholder License Link
+  `;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
