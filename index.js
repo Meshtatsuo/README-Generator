75aebbe1652410f8bde8f0aff73f1321d600b921
MesH <meshtatsuo@gmail.com>
2022-02-21 17:24:06 -0700

    finished prompts

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-
+let projectInfo = [];
+let featureList = [];
 // TODO: Create an array of questions for user input
-const questions = () => {
+const promptProjectInfo = () => {
   inquirer
     .prompt([
       {
@@ -23,7 +24,7 @@ const questions = () => {
         type: "input",
         name: "project-description",
         message: "Please enter a brief description of your project.",
-        validage: (input) => {
+        validate: (input) => {
           if (input) {
             return true;
           } else {
@@ -33,11 +34,58 @@ const questions = () => {
         },
       },
       {
-        // NEXT QUESTION
+        type: "input",
+        name: "install-instructions",
+        message: "Please enter installation instructions.",
+        validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter installation instructions!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Please describe how to use the software.",
+        validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter usage instructions!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "Please provide information regarding making contributions",
+      },
+      {
+        type: "input",
+        name: "test-instructions",
+        message: "Please provide testing instructions.",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Select your project's license",
+        choices: [
+          "Apache License 2.0",
+          "GNU General Public License v3.0",
+          "MIT License",
+          "Boost Software License 1.0",
+          "Creative Commons Zero v1.0 Universal",
+          "Other/None",
+        ],
       },
     ])
     .then((answers) => {
-      // Use user feedback for... whatever!!
+      projectInfo = answers;
+      console.log(projectInfo);
     })
     .catch((error) => {
       if (error.isTtyError) {
@@ -52,7 +100,9 @@ const questions = () => {
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  promptProjectInfo();
+}
 
 // Function call to initialize app
 init();
